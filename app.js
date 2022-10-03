a4f2ff89594e2587495f87136542bcdb1a04227f
niel9537 <niel.zebua8@gmail.com>
2022-10-03 22:10:10 +0700

    add:push project to new repo

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const appRoute = require('./src/routes/route-sparepart');
+app.use('/',appRoute);
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080');
+});
+
+
---
+++ src/routes/route-sparepart.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const {sparepart}   = require('../controllers');
+
+router.get('/sparepart', sparepart.getDataSparepart);
+router.get('/sparepart/:id', sparepart.getDataSparepartByID);
+router.post('/sparepart/add', sparepart.addDataSparepart);
+router.post('/sparepart/edit', sparepart.editDataSparepart);
+router.post('/sparepart/delete', sparepart.deleteDataSparepart);
